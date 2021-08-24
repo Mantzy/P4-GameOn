9e7dd73b893b3e0953aad2249724860a625d7673
Mantzy <fanni.kovacs@hotmail.com>
2021-08-24 13:31:25 +0100

    submit reservation function added

--- js/modal.js
+++ js/modal.js
@@ -28,3 +28,43 @@ modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
 function closeModal() {
     modalbg.style.display = "none"
 }
+
+//functionality to the form inputs
+//validate email
+function ValidateEmail(mail) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)) {
+        return (true)
+    }
+
+    return (false)
+}
+
+const submitBtn = document.querySelectorAll(".btn-submit")
+submitBtn.forEach((btn) => btn.addEventListener("click", submitRes));
+
+function submitRes() {
+    //getting data
+    const reservation = {
+        first: document.getElementById('first').value,
+        last: document.getElementById('last').value,
+        email: document.getElementById('email').value,
+        birthdate: document.getElementById('birthdate').value,
+        quantity: document.getElementById('quantity').value
+            //location: document.getElementById('location').value,
+    }
+
+    //checking if all details are valid
+    if (reservation.first != "" && reservation.last != "" && reservation.birthdate != "" && reservation.quantity != "" && ValidateEmail(reservation.email) != false) {
+        alert('Thank you!')
+
+
+    } else if (reservation.first != "" && reservation.last != "" && reservation.birthdate != "" && reservation.quantity != "" && ValidateEmail(reservation.email) == false) {
+        alert('Please use a valid e-mail address!')
+    } else {
+        alert('Please fill all the fields!')
+    }
+
+
+
+
+}
