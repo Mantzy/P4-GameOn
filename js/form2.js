8738380f143fbed8bb7bbd809e2ad15ed1174ac7
Mantzy <fanni.kovacs@hotmail.com>
2021-08-30 20:58:08 +0100

    validate function fixed

--- js/form2.js
+++ js/form2.js
@@ -13,10 +13,16 @@ function Success() {
 
 }
 
+
+
 const submitBtn = document.querySelectorAll(".btn-submit")
-submitBtn.forEach((btn) => btn.addEventListener("click", validate));
+submitBtn.forEach((btn) => btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    validate();
+}));
 
 function validate() {
+
 
     //getting data
     var reservation = {
@@ -24,13 +30,12 @@ function validate() {
         last: document.getElementById('last').value,
         email: document.getElementById('email').value,
         birthdate: document.getElementById('birthdate').value,
-        quantity: document.getElementById('quantity').value,
-        location: document.getElementsByName('location'),
-        terms: document.getElementById('checkbox1').value
+        quantity: document.getElementById('quantity').value
 
     }
 
     var location = document.reserve.location.value;
+    var terms = document.reserve.checkbox1;
 
 
     var firstError = true;
@@ -45,65 +50,99 @@ function validate() {
     //first name validation
     if (reservation.first == "") {
         document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "Please enter the first name!"
         firstError = true;
-        alert("nope")
+        //alert("nope")
     } else
     if (reservation.first.length < 2) {
         firstError = true;
+        document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "The first name should be more than 2 characters!"
     } else {
-
         firstError = false;
+        document.getElementById('first').style.borderColor = "black";
+        document.getElementById('firstError').innerHTML = ""
     }
 
     //last name validation
     if (reservation.last == "") {
         lastError = true;
+        document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "Please enter the last name!"
     } else if (reservation.last.length < 2) {
+        document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "The last name should be more than 2 characters!"
         lastError = true;
     } else {
         lastError = false;
+        document.getElementById('first').style.borderColor = "black";
+        document.getElementById('firstError').innerHTML = ""
     }
 
     //validate email
     if (reservation.email == "") {
+        document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "Please enter your email address!"
         mailError = true;
     } else if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(reservation.email) === false) {
+        document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "Please enter valid email address!"
         mailError = true;
     } else {
         mailError = false;
+        document.getElementById('first').style.borderColor = "black";
+        document.getElementById('firstError').innerHTML = ""
     }
 
     //validate birth date
     if (reservation.birthdate == "") {
+        document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "Please enter your birth date!"
         birthdateError = true;
     } else {
         birthdateError = false;
+        document.getElementById('first').style.borderColor = "black";
+        document.getElementById('firstError').innerHTML = ""
     }
 
     //validate number of tournaments
     if (reservation.quantity == "") {
+        document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "Please enter a number!"
         quantityError = true;
     } else {
         quantityError = false;
+        document.getElementById('first').style.borderColor = "black";
+        document.getElementById('firstError').innerHTML = ""
     }
 
     //validate location
     if (location == "") {
+        document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "Please choose a location!"
         locationError = true;
     } else {
         locationError = false;
+        document.getElementById('first').style.borderColor = "black";
+        document.getElementById('firstError').innerHTML = ""
     }
 
     //validate terms and condition
-    if (reservation.terms == "") {
+    if (!terms.checked) {
+        document.getElementById('first').style.borderColor = "red";
+        document.getElementById('firstError').innerHTML = "Please accept!"
         termsError = true;
+        alert("term")
     } else {
         termsError = false;
+        document.getElementById('first').style.borderColor = "black";
+        document.getElementById('firstError').innerHTML = ""
     }
 
     if (firstError || lastError || mailError || birthdateError || quantityError || locationError || termsError) {
         return false;
     } else {
+        closeModal();
         Success();
 
         return true;
